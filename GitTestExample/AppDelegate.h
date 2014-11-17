e33b2881adc754c0627091d568e5ebd3ddf51a4f
Kurt McMahon <kmcmahon@niu.edu>
2014-11-17 09:18:00 -0600

    Initial Commit

---
+++ GitTestExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitTestExample
+//
+//  Created by Kurt McMahon on 11/17/14.
+//  Copyright (c) 2014 Northern Illinois University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitTestExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitTestExample
+//
+//  Created by Kurt McMahon on 11/17/14.
+//  Copyright (c) 2014 Northern Illinois University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
