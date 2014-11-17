8acf52537d1cb704c1258a5a117308817492bff7
Kurt McMahon <kmcmahon@niu.edu>
2014-11-17 09:29:00 -0600

    Added MapItinerary class.

---
+++ GitTestExample/MapItinerary.h
@@ -0,0 +1,15 @@
+//
+//  MapItinerary.h
+//  GitTestExample
+//
+//  Created by Kurt McMahon on 11/17/14.
+//  Copyright (c) 2014 Northern Illinois University. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MapItinerary : NSObject
+
+- (void)fakeMethod;
+
+@end
---
+++ GitTestExample/MapItinerary.m
@@ -0,0 +1,18 @@
+//
+//  MapItinerary.m
+//  GitTestExample
+//
+//  Created by Kurt McMahon on 11/17/14.
+//  Copyright (c) 2014 Northern Illinois University. All rights reserved.
+//
+
+#import "MapItinerary.h"
+
+@implementation MapItinerary
+
+- (void)fakeMethod
+{
+    NSLog(@"This method does nothing really");
+}
+
+@end
